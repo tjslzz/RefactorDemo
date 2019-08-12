65429ce9f3fca7230f6b650c92c731603138f660
JarryLi <3244188372@qq.com>
2019-08-12 20:01:09 +0800

    [jerryLi] test : add test for backstate and 40 of quality and 40 of sellin

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -71,4 +71,16 @@ public class GildedRoseTest {
         //then
         assertEquals(41,items[0].quality);
     }
+
+    @Test
+    public void should_return_41_when_call_updateQuality_given_40_of_quality_and_backstage_and_40_of_sellin(){
+        //given
+        items[0] = new Item("Aged Brie",1,40);
+        items[1] = new Item("Backstage passes to a TAFKAL80ETC concert",40,40);
+        GildedRose gildedRose = new GildedRose(items);
+        //when
+        gildedRose.updateQuality();
+        //then
+        assertEquals(41,items[1].quality);
+    }
 }
