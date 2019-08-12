28014d47bfcc44aac5819f18e511ad9c243148c3
FelicityChen <felicity.chen@oocl.com>
2019-08-12 14:18:41 +0800

    test: write test for statement given movie.regular

--- src/test/java/rentalstore/CustomerTest.java
+++ src/test/java/rentalstore/CustomerTest.java
@@ -17,4 +17,14 @@ public class CustomerTest {
         Customer customer = new Customer("Felicity");
         assertEquals("Amount owed is 0.0\nYou earned 0 frequent renter points",customer.footerStr(0.0,0));
     }
+
+    @Test
+    public void should_result_when_call_statement_given_Movie_regular(){
+        Customer customer = new Customer("Jerry");
+        Movie regularMovie = new Movie("FJerry", 0);
+        Rental threeDayRental = new Rental(regularMovie, 3);
+        customer.addRental(threeDayRental);
+
+        assertEquals("Rental Record for Jerry\n\tFJerry\t3.5\nAmount owed is 3.5\nYou earned 1 frequent renter points", customer.statement());
+    }
 }
