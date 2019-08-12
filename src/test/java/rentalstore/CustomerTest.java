ff16ed6f097f818236ca546f4caf9e4f0cfe8fd6
FelicityChen <felicity.chen@oocl.com>
2019-08-12 14:44:57 +0800

    refactor: rename test name more property

--- src/test/java/rentalstore/CustomerTest.java
+++ src/test/java/rentalstore/CustomerTest.java
@@ -7,13 +7,13 @@ import static org.junit.Assert.*;
 public class CustomerTest {
 
     @Test
-    public void should_return_result_when_call_statement() {
+    public void should_return_rentalReciept_when_call_statement() {
         Customer customer = new Customer("Felicity");
         assertEquals("Rental Record for Felicity\nAmount owed is 0.0\nYou earned 0 frequent renter points", customer.statement());
     }
 
     @Test
-    public void should_result_when_call_statement_given_Movie_regular(){
+    public void should_return_rentalReciept_when_call_statement_given_Movie_regular(){
         Customer customer = new Customer("Jerry");
         Movie regularMovie = new Movie("FJerry", 0);
         Rental threeDayRental = new Rental(regularMovie, 3);
@@ -23,7 +23,7 @@ public class CustomerTest {
     }
 
     @Test
-    public void should_result_when_call_statement_given_Movie_new_release(){
+    public void should_return_rentalReciept_when_call_statement_given_Movie_new_release(){
         Customer customer = new Customer("Felicity");
         Movie newReleaseMovie = new Movie("JFelicity", 1);
         Rental twoDayRental = new Rental(newReleaseMovie, 2);
@@ -33,7 +33,7 @@ public class CustomerTest {
     }
 
     @Test
-    public void should_result_when_call_statement_given_Movie_childrens(){
+    public void should_return_rentalReciept_when_call_statement_given_Movie_childrens(){
         Customer customer = new Customer("Felicity");
         Movie childrensMovie = new Movie("JFelicity", 2);
         Rental fourDayRental = new Rental(childrensMovie, 4);
@@ -43,7 +43,7 @@ public class CustomerTest {
     }
 
     @Test
-    public void should_result_when_call_statement_given_Movie_childrens_getDayRented_2(){
+    public void should_return_rentalReciept_when_call_statement_given_Movie_childrens_getDayRented_2(){
         Customer customer = new Customer("Felicity");
         Movie childrensMovie = new Movie("JFelicity", 2);
         Rental fourDayRental = new Rental(childrensMovie, 2);
@@ -53,13 +53,13 @@ public class CustomerTest {
     }
 
     @Test
-    public void should_return_footer_when_call_footer_str(){
+    public void should_return_rentalReciept_footer_when_call_footer_str(){
         Customer customer = new Customer("Felicity");
         assertEquals("Amount owed is 0.0\nYou earned 0 frequent renter points",customer.footerStr(0.0,0));
     }
 
     @Test
-    public void should_return_result_when_call_statement_given_Movie_regular_getDayRented_1() {
+    public void should_return_rentalReciept_result_when_call_statement_given_Movie_regular_getDayRented_1() {
         Customer customer = new Customer("Jerry");
         Movie regularMovie = new Movie("FJerry", 0);
         Rental threeDayRental = new Rental(regularMovie, 1);
