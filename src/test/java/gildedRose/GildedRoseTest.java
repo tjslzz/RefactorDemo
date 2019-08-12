1835abe0ba5144abae7f0ca7ab3021b64cdad893
JarryLi <3244188372@qq.com>
2019-08-12 20:12:27 +0800

    [jerryLi] test : add test for aged brie and -1 of sellin

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -127,4 +127,15 @@ public class GildedRoseTest {
         //then
         assertEquals(1,items[0].sellIn);
     }
+    @Test
+    public void should_return_42_quality_when_call_updateQuality_given_40_of_quality_and_backstage_and_1_of_sellin(){
+        //given
+        items[0] = new Item("Aged Brie",-1,40);
+        items[1] = new Item("Backstage passes to a TAFKAL80ETC concert",40,40);
+        GildedRose gildedRose = new GildedRose(items);
+        //when
+        gildedRose.updateQuality();
+        //then
+        assertEquals(42,items[0].quality);
+    }
 }
